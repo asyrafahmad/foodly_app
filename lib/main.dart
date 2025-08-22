b7fb3b1b67a933ced0b913ca0409db421bfee03d
asyrafa <asyrafa@sunway.edu.my>
2025-08-22 14:36:21 +0800

    [Add] entrypoint PageList

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:foodly_app/constants/constants.dart';
 import 'package:foodly_app/views/entrypoint.dart';
 import 'package:get/get.dart';
 
-Widget defaultHome = const MainScreen();
+Widget defaultHome = MainScreen();
 
 void main() {
   runApp(const MyApp());
--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,60 +1,81 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:foodly_app/constants/constants.dart';
+import 'package:foodly_app/controller/tab_index_controller.dart';
+import 'package:foodly_app/views/cart/cart_page.dart';
+import 'package:foodly_app/views/home/home_page.dart';
+import 'package:foodly_app/views/profile/profile_page.dart';
+import 'package:foodly_app/views/search/search_page.dart';
+import 'package:get/get.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+  MainScreen({super.key});
+
+  final controller = Get.put(TabIndexController());
+
+  List<Widget> pageList = const [
+    // Add your pages here
+    // Example: HomePage(), SearchPage(), CartPage(), ProfilePage()
+    HomePage(),
+    SearchPage(),
+    CartPage(),
+    ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // Background image or color
-          Container(
-            height: height,
-            width: width,
-            color: kOffWhite,
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Theme(
-              data: Theme.of(context).copyWith(canvasColor: kPrimary),
-              child: BottomNavigationBar(
-                showSelectedLabels: true,
-                showUnselectedLabels: false,
-                unselectedIconTheme: const IconThemeData(
-                  color: kDark,
+
+    return Obx(() =>  Scaffold(
+        body: Stack(
+          children: [
+            pageList[controller.tabIndex],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Theme(
+                data: Theme.of(context).copyWith(canvasColor: kPrimary),
+                child: BottomNavigationBar(
+                  showSelectedLabels: true,
+                  showUnselectedLabels: false,
+                  unselectedIconTheme: const IconThemeData(
+                    color: kDark,
+                  ),
+                  selectedIconTheme: const IconThemeData(
+                    color: kWhite,
+                  ),
+                  onTap: (value) {
+                    // Handle navigation logic here
+                    controller.setTabIndex = value;
+                  },
+                  currentIndex: controller.tabIndex,
+                  items: [
+                    BottomNavigationBarItem(
+                      icon: controller.tabIndex == 0 ? Icon(AntDesign.appstore1) : Icon(AntDesign.appstore_o),
+                      label: 'Home',
+                    ),
+                    const BottomNavigationBarItem(
+                      icon: Icon(Icons.search),
+                      label: 'Search',
+                    ),
+                    const BottomNavigationBarItem(
+                      icon: Badge(
+                        label: Text('1'),
+                        child: Icon(FontAwesome.opencart)
+                      ),
+                      label: 'Cart',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: controller.tabIndex == 3 ? Icon(FontAwesome.user_circle) : Icon(FontAwesome.user_circle_o),
+                      label: 'Profile',
+                    ),
+                  ]
                 ),
-                selectedIconTheme: const IconThemeData(
-                  color: kWhite,
-                ),
-                onTap: (value) {
-                  // Handle navigation logic here
-                  print("Selected index: $value");
-                },
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.search),
-                    label: 'Search',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.shopping_cart),
-                    label: 'Cart',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.person),
-                    label: 'Profile',
-                  ),
-                ]
               ),
-            ),
-          )
-        ],
-      ),
+            )
+            ],
+        ),
+      )
     );
   }
 }
