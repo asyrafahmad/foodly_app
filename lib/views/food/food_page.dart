f2bd45e8b5ebfdd0d6c9577b9d95bd75f640dbdb
asyrafa <asyrafa@sunway.edu.my>
2026-01-06 16:07:47 +0800

    [Add] Title text and description

--- lib/views/food/food_page.dart
+++ lib/views/food/food_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:foodly_app/common/app_style.dart';
 import 'package:foodly_app/common/custom_button.dart';
+import 'package:foodly_app/common/reusable_text.dart';
 import 'package:foodly_app/constants/constants.dart';
 import 'package:foodly_app/controller/food_controller.dart';
 import 'package:foodly_app/hooks/fetch_restaurant.dart';
@@ -124,6 +126,30 @@ class _FoodPageState extends State<FoodPage> {
             )
           ),
 
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.w),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ReusableText(text: widget.food.title, style: appStyle(18, kDark, FontWeight.w600)),
+                      ReusableText(text: "RM ${widget.food.price.toStringAsFixed(2)}", style: appStyle(18, kPrimary, FontWeight.w600)),
+                    ],
+                  ),
+                  SizedBox(height: 5.h),
+                  Text(
+                    widget.food.description, 
+                    textAlign: TextAlign.justify,
+                    style: appStyle(14, kGray, FontWeight.w400)
+                  ),
+                ],
+              ),
+            )
+          )
           
         ],
       ),
